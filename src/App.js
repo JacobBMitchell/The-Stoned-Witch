19444c44addb54b239307455719e2b8dba4e985e
JacobBMitchell <mattdamionoriginal@gmail.com>
2024-05-01 22:59:04 -0500

    adding nav bar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import './App.css';
 import './HomePage'
 import HomePage from './HomePage';
@@ -7,11 +7,16 @@ import Nav from './Nav';
 
 function App() {
   return (
-    <div className="App">
-      <Nav/>
-      <HomePage/>
-      <Shop/>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Nav/>
+        <Routes>
+          <Route path="/" element={<HomePage/>}></Route>
+          <Route path="/shop" element={<Shop/>}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+    
   );
 }
 
