30883535d19b7482f36b9186a220e7ee6c697aa5
JacobBMitchell <mattdamionoriginal@gmail.com>
2024-05-01 22:18:25 -0500

    Installed react router dom

---
+++ src/HomePage.js
@@ -0,0 +1,5 @@
+function HomePage() {
+    return (<h1>Hi Carol</h1>)
+}
+
+export default HomePage;
---
+++ src/Nav.js
@@ -0,0 +1,7 @@
+function Nav() {
+    return <nav>
+        <link to="/">The Stoned Witch</link>
+    </nav>
+}
+
+export default Nav;
